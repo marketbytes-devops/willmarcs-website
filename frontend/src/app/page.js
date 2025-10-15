0edb8a796a9955dec9c2f283d1dfe54d72631e0a
ajay-marketbytes <ajay@marketbytes.in>
2025-10-15 10:28:18 +0530

    Updates v1

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1543,8 +1543,8 @@ export default function Home() {
               />
             </div>
             <div className="w-full pb-4 sm:pb-16 pt-8">
-              <Slider 
-              className="slick-slider"
+              <Slider
+                className="slick-slider"
                 {...{
                   dots: true,
                   infinite: true,
@@ -1555,12 +1555,26 @@ export default function Home() {
                   autoplaySpeed: 5000,
                   pauseOnHover: true,
                   arrows: false,
+                  centerMode: false,
+                  variableWidth: false,
                   responsive: [
                     {
                       breakpoint: 1024,
                       settings: {
-                        slidesToShow: 2,
+                        slidesToShow: 3,
                         slidesToScroll: 1,
+                        centerMode: false,
+                      },
+                    },
+                    {
+                      breakpoint: 768,
+                      settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 1,
+                        dots: true,
+                        arrows: false,
+                        centerMode: false,
+                        centerPadding: "0px",
                       },
                     },
                     {
@@ -1570,8 +1584,8 @@ export default function Home() {
                         slidesToScroll: 1,
                         dots: true,
                         arrows: false,
-                        centerMode: true,
-                        centerPadding: "20px",
+                        centerMode: false,
+                        centerPadding: "0px",
                       },
                     },
                   ],
