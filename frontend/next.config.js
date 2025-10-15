49431861607276196092d3b7e0582ebc39bfe305
ajay-marketbytes <ajay@marketbytes.in>
2025-10-15 09:55:23 +0530

    Updates final v6

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', 
+  basePath: '/landing',
+
   webpack: (config) => {
     config.module.rules.push({
       test: /\.(mp4|webm)$/,
