1cbfbe9f9c77ec1a0c75bc9b74bdb25af0e1d910
ajay-marketbytes <ajay@marketbytes.in>
2025-10-15 17:46:54 +0530

    Updates v7

--- frontend/src/components/Progess/index.jsx
+++ frontend/src/components/Progess/index.jsx
@@ -47,13 +47,27 @@ const Progress = () => {
     dots: false,
     infinite: true,
     slidesToShow: 2,
-    slidesToScroll: 2,
+    slidesToScroll: 1,
     autoplay: true,
     speed: 3000,
     autoplaySpeed: 1000,
     cssEase: 'linear',
     pauseOnHover: true,
-    arrows: false
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+    ],
   };
 
   return (
